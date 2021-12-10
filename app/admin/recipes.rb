034a774485cf05e8b6b8e65b4043ef080a79bb2a
Jack Cavanaugh <jcavanaugh05@gmail.com>
2021-12-10 20:17:10 +0000

    Whitelist columns in Recipe for ActiveAdmin

--- app/admin/recipes.rb
+++ app/admin/recipes.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Recipe do
 
+ permit_params :business_id, :recipe_name, :recipe_description
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
