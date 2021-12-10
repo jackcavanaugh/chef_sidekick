776c9989f41ec1aac0926fade6a90e10619fba08
Jack Cavanaugh <jcavanaugh05@gmail.com>
2021-12-10 20:17:37 +0000

    Add foreign key ingredient_reviewer reference to ingredient_reviews table

---
+++ db/migrate/20211210201737_add_ingredient_reviewers_reference_to_ingredient_reviews.rb
@@ -0,0 +1,7 @@
+class AddIngredientReviewersReferenceToIngredientReviews < ActiveRecord::Migration[6.0]
+  def change
+    add_foreign_key :ingredient_reviews, :business_accounts, column: :ingredient_reviewer
+    add_index :ingredient_reviews, :ingredient_reviewer
+    change_column_null :ingredient_reviews, :ingredient_reviewer, false
+  end
+end
