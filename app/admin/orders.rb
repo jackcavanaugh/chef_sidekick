69389a84204d0541d660e8d1006897add26e5fa6
Jack Cavanaugh <jcavanaugh05@gmail.com>
2021-12-10 20:17:23 +0000

    Whitelist columns in Order for ActiveAdmin

--- app/admin/orders.rb
+++ app/admin/orders.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Order do
 
+ permit_params :recipe_id, :customer_comments
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
