cd1eceeba8650dc6648cff5984e747396cfca725
Jack Cavanaugh <jcavanaugh05@gmail.com>
2021-12-10 20:17:16 +0000

    Whitelist columns in SupplierReview for ActiveAdmin

--- app/admin/supplier_reviews.rb
+++ app/admin/supplier_reviews.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register SupplierReview do
 
+ permit_params :reviewer, :supplier_id, :review_title, :review_rating, :review_content
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
