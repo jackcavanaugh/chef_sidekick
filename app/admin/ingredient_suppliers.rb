b31638c1a9b0b1e466501fc1147d0160ce087d81
Jack Cavanaugh <jcavanaugh05@gmail.com>
2021-12-10 20:17:12 +0000

    Whitelist columns in IngredientSupplier for ActiveAdmin

--- app/admin/ingredient_suppliers.rb
+++ app/admin/ingredient_suppliers.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register IngredientSupplier do
 
+ permit_params :supplier_name, :supplier_description, :supplier_location
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
